005f181a9ff5dd484d3669a1a0ad39b87f3bdc8a
ALVIN JEFFREY DELOS REYES <ajseyersoled25@gmail.com>
2025-01-04 15:54:02 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          className="App-link"
+          src="https://firebasestorage.googleapis.com/v0/b/capstone-178d3.appspot.com/o/comfort-food%2FFINAL%20JDB%20MENU%20PAGE%201.png?alt=media&token=036c557d-a4c5-4ceb-8319-e4c9f139d48f"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+        <img
+          className="App-link"
+          src="https://firebasestorage.googleapis.com/v0/b/capstone-178d3.appspot.com/o/comfort-food%2FFINAL%20JDB%20MENU%20PAGE%202.png?alt=media&token=1e283577-c31d-48ef-a473-20640ca7fca7"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
